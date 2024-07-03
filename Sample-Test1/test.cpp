d20c990f2e0cb7f6b8e3ae0881f08b0f4bef26cd
JJY <binaryfalse81@gmail.com>
2024-07-03 11:06:38 +0900

    38

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,14 +3,19 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, 0f1) {
+class PrimeFixture : public testing::Test
+{
+public:
     PrimeFactor prime_factor;
-    vector<int> expected = {};
+    vector<int> expected;
+};
+
+TEST_F(PrimeFixture, 0f1) {
+    expected = {};
     EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, 0f2) {
-    PrimeFactor prime_factor;
-    vector<int> expected = {2};
+TEST_F(PrimeFixture, 0f2) {
+    expected = {2};
     EXPECT_EQ(expected, prime_factor.of(2));
 }
