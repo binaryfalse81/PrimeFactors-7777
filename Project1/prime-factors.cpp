316bf6fd16083c5676f5570fff14eb61d344d789
JJY <binaryfalse81@gmail.com>
2024-07-03 11:04:14 +0900

    37

--- Project1/prime-factors.cpp
+++ Project1/prime-factors.cpp
@@ -4,9 +4,13 @@ using namespace std;
 class PrimeFactor
 {
 public:
-    vector<int> of(int i)
+    vector<int> of(int number)
     {
         vector<int> result = {};
+        if (number == 2)
+        {
+            result.push_back(2);
+        }
         return result;
     }
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -8,3 +8,9 @@ TEST(PrimeFactors, 0f1) {
     vector<int> expected = {};
     EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, 0f2) {
+    PrimeFactor prime_factor;
+    vector<int> expected = {2};
+    EXPECT_EQ(expected, prime_factor.of(2));
+}
