8effa7aa9e9dabedb79655a8521f2d80ebf58c44
JJY <binaryfalse81@gmail.com>
2024-07-03 11:24:18 +0900

    56

--- Project1/prime-factors.cpp
+++ Project1/prime-factors.cpp
@@ -10,15 +10,7 @@ public:
         if (number >  1)
         {
             int divisor = 2;
-            if (number == 4)
-            {
-                while (number % divisor == 0)
-                {
-                    result.push_back(divisor);
-                    number /= divisor;
-                }
-            }
-            else if (number == 6)
+            if (number == 4 || number == 6 || number == 9)
             {
                 for (divisor = 2; number > 1; divisor++)
                 {
@@ -28,11 +20,6 @@ public:
                         number /= divisor;
                     }
                 }
-            }
-            else if (number == 9)
-            {
-                result.push_back(3);
-                result.push_back(3);
             }
             else
             {
